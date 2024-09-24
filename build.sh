56be87151a5ca3c83af52a1c9cf33fd411a9d4d9
Linh B Ngo <lngo@wcupa.edu>
2024-09-24 16:47:37 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+docker image rm spark-worker | true
+docker image rm spark-master | true
+
+dos2unix docker/spark-worker/start-worker.sh
+dos2unix docker/spark-master/start-master.sh
+
+docker build -t spark-master:latest ./docker/spark-master
+docker build -t spark-worker:latest ./docker/spark-worker
+
+echo "Launch the Dockerized Spark cluster with a specific number of workers"
+echo "docker-compose up -d --scale spark-worker=  "
