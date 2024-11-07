648dcd43b6a22ef247813ab4c9537ca29a333104
Linh B Ngo <lngo@wcupa.edu>
2024-11-07 16:12:45 -0500

    Update build.bat

--- build.bat
+++ build.bat
@@ -4,4 +4,3 @@ docker build -t spark-master:latest ./docker/spark-master
 docker build -t spark-worker:latest ./docker/spark-worker
 echo "Launch the Dockerized Spark cluster with a specific number of workers"
 echo "docker-compose up -d --scale spark-worker=  "
-pause
